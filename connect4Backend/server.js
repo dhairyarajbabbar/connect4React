59f11b1912b5a11492429cfdc5e4c7f1b8775d20
dhairyarajbabbar <dhairyarajbabbar@gmail.com>
2024-02-02 23:24:36 +0530

    included frontend domain in cors

--- connect4Backend/server.js
+++ connect4Backend/server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname,"build")));
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://connect4-react.vercel.app/",
     methods: ["GET", "POST"],
     credentials: true,
   },
